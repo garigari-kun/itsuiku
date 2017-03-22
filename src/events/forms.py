5f83c267e04e2d51ab94979fea5c77b846ec5922
kei-cs <keisuke.cs@gmail.com>
2017-03-22 17:53:54 +0900

    Tried formset ver00

--- src/events/forms.py
+++ src/events/forms.py
@@ -4,15 +4,37 @@ from .models import Event, Schedule
 
 
 class EventModelForm(forms.ModelForm):
+
+    title = forms.CharField(
+        label="タイトル",
+        required=True,
+    )
+
+    description = forms.CharField(
+        label="メモ"
+    )
+
     class Meta:
         model = Event
-        fields = [
-        'title',
-        'description'
-        ]
+        fields = ['title', 'description']
+
 
 
 class ScheduleModelForm(forms.ModelForm):
+
+    date = forms.CharField(
+        label='日付'
+    )
+
+    start_time = forms.CharField(
+        label='開始時間',
+
+    )
+
+    end_time = forms.CharField(
+        label='終了時間'
+    )
+
     class Meta:
         model = Schedule
         fields = [
@@ -20,3 +42,13 @@ class ScheduleModelForm(forms.ModelForm):
             'start_time',
             'end_time'
         ]
+
+# birth_date = forms.DateField(
+#     label='生年月日',
+#     required=False,
+#     widget=SelectDateWidget(
+#         attrs={'class': 'form-control'},
+#         years=range(1900, now.year + 1),
+#         months=MONTHS,
+#     ),
+# )
--- src/events/views.py
+++ src/events/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.base import TemplateView, View
+from django.views.generic.edit import CreateView
+from django.forms import formset_factory
 
 from .forms import EventModelForm, ScheduleModelForm
 from .models import Event, Schedule
@@ -24,11 +27,41 @@ class CreateEventView(View):
         template_name = 'events/create_event.html'
         # form
         event_form = EventModelForm()
-        schedule_form = ScheduleModelForm()
+
+        ScheduleFormSet = formset_factory(ScheduleModelForm, extra=9)
+        schedule_formset = ScheduleFormSet()
 
         context = {
             'event_form': event_form,
-            'schedule_form': schedule_form,
+            'schedule_formset': schedule_formset
         }
 
         return render(request, template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        event_form = EventModelForm(request.POST or None)
+        ScheduleFormSet = formset_factory(ScheduleModelForm, extra=9)
+        schedule_formset = ScheduleFormSet(request.POST or None)
+        if event_form.is_valid() and schedule_formset.is_valid():
+            print('Eventform')
+            print(repr(event_form))
+            print('ScheduleFormSet')
+            print(repr(ScheduleFormSet))
+        else:
+            print('Problem occured')
+        return HttpResponse('OK')
+
+
+
+
+
+class CreateEventViewWIP(CreateView):
+    pass
+    """
+    Research
+    1. Can I use two differenct ModelForm for this view?
+    2. If so, can I name these differently and implement these separately???
+    3. 1 can't be done, are there any good way of solving these? Just View is good choice?
+
+
+    """
