5a81b4d832161957deb252ee402ab38549338c26
kei-cs <keisuke.cs@gmail.com>
2017-03-25 22:04:09 +0900

    WIP attendances view

--- src/attendances/views.py
+++ src/attendances/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from events.models import Event
+
+class EventTopView(View):
+    # test event code : dcmqrez5b591
+    def get(self, request, event_code=None, *args, **kwargs):
+        template_name = 'events/event-top.html'
+        event = get_object_or_404(Event, event_code=event_code)
+        context = {
+            'event': event,
+
+        }
+        print(event_code)
+        return render(request, template_name, context)
--- src/events/urls.py
+++ src/events/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
 
 from .views import (
-    # EventTopView,
-    EventTopView,
-    DashboardView,
     CreateEventView
 )
 
