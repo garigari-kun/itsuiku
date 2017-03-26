7f194dcb88c4476594bdef68b29e0b6bec80e8a0
kei-cs <keisuke.cs@gmail.com>
2017-03-27 08:20:21 +0900

    Created base view for accounts

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.base import TemplateView, View
 
-# Create your views here.
+
+class LogInView(View):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+
+
+class LogOutView(View):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+        
