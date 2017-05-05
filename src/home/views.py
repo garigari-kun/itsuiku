0254f42c8588cf11596f83a98702f60180cf7381
kei-cs <keisuke.cs@gmail.com>
2017-05-05 22:29:54 +0900

    Reimplemented TopView

--- src/home/views.py
+++ src/home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, View
 
 
-class TopView(TemplateView):
-    template_name = 'home/index.html'
+# class TopView(TemplateView):
+#     template_name = 'home/index.html'
+
+
+class TopView(View):
+
+    def get(self, request, *args, **kwargs):
+        template_name = 'home/index.html'
+        context = {}
+        return render(request, template_name, context)
