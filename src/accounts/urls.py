8faaeb0e2f3050c62ab901c284ce6326ff30de57
kei-cs <keisuke.cs@gmail.com>
2017-05-12 22:43:23 +0900

    Created User Signup view

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import LoginView
+from .views import LoginView, SignUpView
 
 
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^signup/$', SignUpView.as_view(), name='signup'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView, View
 
+from .forms import EmailUserForm
+
 
 class LoginView(View):
     def get(self, request, *args, **kwargs):
@@ -17,7 +19,12 @@ class LoginView(View):
 
 class SignUpView(View):
     def get(self, request, *args, **kwargs):
-        pass
+        template_name = 'accounts/signup.html'
+        form = EmailUserForm()
+        context = {
+            'form': form,
+        }
+        return render(request, template_name, context)
 
     def post(self, request, *args, **kwargs):
         pass
@@ -31,3 +38,41 @@ class LogOutView(View):
 
     def post(self, request, *args, **kwargs):
         pass
+
+
+
+
+# def register(request):
+#     template_name = 'registration/register.html'
+#     if request.method == 'POST':
+#         is_success, context = register_post(request)
+#         if is_success:
+#             return redirect('dashboard:dashboard')
+#     else:
+#         form = EmailUserForm()
+#         context = {
+#             'form': form,
+#         }
+#     return render(request, template_name, context)
+#
+#
+# def register_post(request):
+#     form = EmailUserForm(request.POST)
+#     is_success = False
+#     context = {}
+#     if form.is_valid():
+#         new_user = form.save(commit=False)
+#         email = form.cleaned_data['email']
+#         password = form.cleaned_data['password1']
+#         new_user.save()
+#         login_user = authenticate(email=email, password=password)
+#         if login_user is not None:
+#             if login_user.is_active:
+#                 login(request, login_user)
+#                 is_success = True
+#                 return is_success, context
+#     else:
+#         context = {
+#             'form': form,
+#         }
+#     return is_success, context
