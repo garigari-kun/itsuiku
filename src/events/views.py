a1c496cd6f39b31ff610d3f42cf512c087aca3d9
kei-cs <keisuke.cs@gmail.com>
2017-06-11 21:43:46 +0900

    Tried UpdateEventView

--- src/events/views.py
+++ src/events/views.py
@@ -122,11 +122,17 @@ class UpdateEventView(View):
         #
         # return render(request, self.template_name, context)
         event_form = self.get_event_form(request, instance=event)
-        schedule_formset = self.get_schedule_formset(request, extra=0, instance=event)
+        schedule_list = []
+        for schedule in event.schedule_range.all():
+            schedule_list.append(schedule)
+        print(schedule_list)
+
+        schedule_formset = self.get_schedule_formset(request, instance=event.schedule_range.all())
+        # schedule_formset = self.get_schedule_formset(request, instance=schedule_list)
 
         context = {
             'event_form': event_form,
-            # 'schedule_formset': schedule_formset
+            'schedule_formset': schedule_formset
         }
         return render(request, self.template_name, context)
 
@@ -140,15 +146,13 @@ class UpdateEventView(View):
         return form
 
     def get_schedule_formset(self, request, extra=0, instance=None, *args, **kwargs):
-        ScheduleFormSet = formset_factory(ScheduleModelForm, extra=extra)
-        formset = ScheduleFormSet(request.POST or None, instance=instance)
+        ScheduleFormSet = formset_factory(ScheduleModelForm)
+        formset = ScheduleFormSet(request.POST or None, initial=instance)
+        # formset = ScheduleFormSet(request.POST or None, queryset=instance)
+
         return formset
 
 
-    # def get_schedule_formset(self, request, extra=0, instance, *args, **kwargs):
-    #     ScheduleFormSet = formset_factory(ScheduleModelForm, extra=extra)
-    #     formset = ScheduleFormSet(request.POST or None, instance=instance)
-    #     return formset
 
 
 
@@ -167,3 +171,49 @@ class DeleteEventView(LoginRequiredMixin, View):
             print(schedule)
         event.delete()
         return redirect('user-dashboard')
+
+
+
+
+
+# # UpdateEventView TEST
+# class UpdateEventView(View):
+#
+#     template_name = 'create_event.html'
+#
+#     def get(self, request, pk=None, *args, **kwargs):
+#         event = get_object_or_404(Event, pk=pk)
+#         event_form = self.get_event_form(request, instance=event)
+#         schedule_list = []
+#         for schedule in event.schedule_range.all():
+#             dictio = {
+#                 'date': schedule.date,
+#                 'comment': schedule.comment
+#             }
+#             print(dictio)
+#             schedule_list.append(dictio)
+#         print(schedule_list)
+#
+#         schedule_formset = self.get_schedule_formset(request, instance=schedule_list)
+#         # schedule_formset = self.get_schedule_formset(request, instance=schedule_list)
+#
+#         context = {
+#             'event_form': event_form,
+#             'schedule_formset': schedule_formset
+#         }
+#         return render(request, self.template_name, context)
+#
+#         # return HttpResponse('update view')
+#
+#     def post(self, request, event_code=None, *args, **kwargs):
+#         pass
+#
+#     def get_event_form(self, request, instance, *args, **kwargs):
+#         form = EventModelForm(request.POST or None, instance=instance)
+#         return form
+#
+#     def get_schedule_formset(self, request, extra=0, instance=None, *args, **kwargs):
+#         ScheduleFormSet = formset_factory(ScheduleModelForm, extra=0)
+#         formset = ScheduleFormSet(request.POST or None, initial=instance)
+#
+#         return formset
