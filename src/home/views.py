9dba495e36103ae8cff90ad0533b00b59074db97
kei-cs <keisuke.cs@gmail.com>
2017-07-08 16:25:08 +0900

    Setting static files

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView, View
+from django.views.generic.base import View
 
 
 class TopView(View):
--- src/itsuiku/settings.py
+++ src/itsuiku/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 # events app global
 EVENT_CODE_LEN = 12
