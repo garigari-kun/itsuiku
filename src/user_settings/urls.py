33e41b517f4ffdb198f863b3cc94ec1a9acb32df
kei-cs <keisuke.cs@gmail.com>
2017-07-17 19:38:34 +0900

    Preped UserPasswordChangeView

--- src/user_settings/urls.py
+++ src/user_settings/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 
 from .views import (
     UserSettingsView,
-    DeleteUserAccount
+    DeleteUserAccount,
+    UserPasswordChangeView
 )
 
 urlpatterns = [
     url(r'^$', UserSettingsView.as_view(), name='main'),
+    url(r'^change_password$', UserPasswordChangeView.as_view(), name='change_password'),
     url(r'^delete$', DeleteUserAccount.as_view(), name='delete')
 ]
--- src/user_settings/views.py
+++ src/user_settings/views.py
@@ -52,6 +52,26 @@ class UserSettingsView(View):
 
 
 
+class UserPasswordChangeView(View):
+
+    def get(self, request, *args, **kwargs):
+        template_name = self.get_template_name(request)
+        context = self.get_context_data(request)
+        return render(request, template_name, context)
+
+
+    def get_context_data(self, request, *args, **kwargs):
+        context = {}
+        return context
+
+
+    def get_template_name(self, request, *args, **kwargs):
+        template_name = 'user_settings/change_password.html'
+        return template_name
+
+
+
+
 class DeleteUserAccount(View):
 
     def get(self, request, *args, **kwargs):
