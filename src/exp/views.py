b75e90f583b051e712888ab0572f331b2938b709
kei-cs <keisuke.cs@gmail.com>
2017-08-19 22:31:21 +0900

    Improved done_deleting.html

--- src/exp/views.py
+++ src/exp/views.py
@@ -2,18 +2,16 @@ from django.shortcuts import render
 
 from django.views.generic.base import View
 
-from events.models import Event, Schedule
-from events.forms import EventModelForm, ScheduleModelForm, ScheduleDeletionCheckMOdelForm
 
 
 
-class FormExpView(View):
+class ExpView(View):
 
-    template_name = ''
+    template_name = 'user_settings/done_deleting.html'
 
 
     def get(self, request, *args, **kwargs):
-        pass
+        return render(request, self.template_name, {})
 
     def post(self, request, *args, **kwargs):
         pass
